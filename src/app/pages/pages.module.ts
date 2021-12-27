130b1a137494164c21fb8edca2fc120318c9f1e9
Emmanuel Cortes <polosetroc@gmail.com>
2021-12-27 13:37:53 -0800

    Added content to PageNotFound and WelcomePage components.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {ProfileComponent} from "./profile/profile.component";
 import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
 import {TaskModule} from "./task/task.module";
-import {LoginModule} from "./login/login.module";
 import {SharedModule} from "../shared/shared.module";
 
 
@@ -12,7 +10,6 @@ import {SharedModule} from "../shared/shared.module";
 @NgModule({
   declarations: [
     PageNotFoundComponent,
-    ProfileComponent,
     WelcomePageComponent,
   ],
   imports: [
@@ -20,10 +17,8 @@ import {SharedModule} from "../shared/shared.module";
   ],
   exports: [
     PageNotFoundComponent,
-    ProfileComponent,
     WelcomePageComponent,
     TaskModule,
-    LoginModule,
   ],
 })
 export class PagesModule { }
